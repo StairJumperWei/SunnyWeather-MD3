598fbc1f69b6f46a52c8b0e787936b4761dd2bfe
StairJumperWei <320876266@qq.com>
2022-05-11 13:01:41 +0800

    First commit.

---
+++ app/src/main/java/com/sunnyweather/sunnyweather_md3/SunnyWeatherApplication.kt
@@ -0,0 +1,18 @@
+package com.sunnyweather.sunnyweather_md3
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application(){
+    companion object{
+        const val TOKEN = ""
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
---
+++ app/src/main/java/com/sunnyweather/sunnyweather_md3/logic/network/PlaceService.kt
@@ -0,0 +1,12 @@
+package com.sunnyweather.sunnyweather_md3.logic.network
+
+import com.sunnyweather.sunnyweather_md3.SunnyWeatherApplication
+import com.sunnyweather.sunnyweather_md3.logic.model.PlaceResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PlaceService {
+    @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
+    fun searchPlace(@Query("query") query: String): Call<PlaceResponse>
+}
